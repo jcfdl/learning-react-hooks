78a43adba472ec2623ad74d113783adf21205b8d
jcfdl <jc.deleon027@gmail.com>
2020-04-13 15:23:35 +0800

    usestate on object

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import ClassCounter from './components/ClassCounter'
 import HookCounter from './components/HookCounter'
+import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter />
+      <HookCounterThree />
+      {/* <HookCounterTwo /> */}
+      {/* <HookCounter /> */}
       {/*<ClassCounter />*/}
     </div>
   );
